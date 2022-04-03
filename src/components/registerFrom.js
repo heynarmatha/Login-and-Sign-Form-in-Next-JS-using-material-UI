4af1ec5dc10c9f3ef06cf4c4d7b0d874aa581a14
heynarmatha <hey.narmatha@gmail.com>
2022-04-03 16:59:14 +0530

    second commit

--- src/components/registerFrom.js
+++ src/components/registerFrom.js
@@ -1,47 +1,58 @@
-import { Box, Typography } from "@mui/material";
-import { padding } from "@mui/system";
-
-const RegisterFrom = () => {
+import { Box, Typography, Grid,Card,CardMedia } from "@mui/material";
+function RegisterFrom() {
   return (
-    <>
-      <Box
+    <Grid container direction="row">
+      <Grid item sm={3} lg={1.7}>
+        <Box
+          sx={{
+            color: "black",
+            height: "100vh",
+            padding: "20px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box display="flex" alignItems="center">
+            <Typography
+              variant="h6"
+              sx={{
+                backgroundColor: "primary.main",
+                color: "white",
+                padding: "5px 10px",
+                borderRadius: "10px",
+              }}
+            >
+              {" "}
+              Ua
+            </Typography>
+            <Typography variant="h5" ml={1}>
+              {" "}
+              UI Analysis
+            </Typography>
+          </Box>
+
+          <Typography variant="subtitle2" component="h1" fontSize="30px">
+            Make your Interface review easier
+          </Typography>
+
+          <Typography>Image</Typography>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        lg={10.3}
+        sm={9}
         sx={{
           backgroundColor: "primary.main",
-          color: "white",
-          height: "100vh",
-          width: "250px",
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
         }}
       >
-        <Box display="flex" alignItems="center">
-          <Typography
-            variant="h6"
-            sx={{
-              backgroundColor: "white",
-              color: "black",
-              padding: "5px 10px",
-              borderRadius:"10px",
-            }}
-          >
-            {" "}
-            Ua
-          </Typography>
-          <Typography variant="h5" ml={1}>
-            {" "}
-            UI Analysis
-          </Typography>
-        </Box>
+        <Box>
 
-        <Typography variant="subtitle2" component="h1" fontSize="30px">
-          Make your Interface review easier
-        </Typography>
-        <Typography>image</Typography>
-      </Box>
-    </>
+        </Box>
+      </Grid>
+    </Grid>
   );
-};
+}
 
 export default RegisterFrom;
